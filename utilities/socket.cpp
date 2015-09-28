0d220ff46e851e7c678aee5e26e6c59ac5a75411
Alessio <alebnt@gmail.com>
2015-09-28 13:55:49 +0200

    2 funzioni aggiunte socket.cpp

--- utilities/socket.cpp
+++ utilities/socket.cpp
@@ -7,15 +7,32 @@
 
 #include "socket.h"
 
-namespace utilities {
+using namespace utilities;
 
-socket::socket() {
-	// TODO Auto-generated constructor stub
+/**
+ * Descr
+ * @param af
+ * @param type
+ * @param protocol
+ */
+socket_base::socket_base(int af, int type, int protocol):
+		handle(::socket(af, type, protocol)), blocking(true){}
+
+socket_listener::socket_listener(int af, int type, int protocol, uint32_t ip, in_port_t port){
+	struct sockaddr_in addr = {0};
+	socklen_t addr_len;
+
+	addr_len = sizeof(struct sockaddr_in);
+
+	addr.sin_family = af;
+	addr.sin_addr.s_addr = ip;
+	addr.sin_port = htons(port);
+
+	bind(handle, (struct sockaddr*)&addr, addr_len);
+}
+
+socket_stream&& accept(){
 
 }
 
-socket::~socket() {
-	// TODO Auto-generated destructor stub
-}
-
-} /* namespace utilities */
+/* namespace utilities */
