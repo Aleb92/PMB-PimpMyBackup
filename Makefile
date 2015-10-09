d91cfc58e296628578500758e09305eb3d57b741
Marco Manino <mrcmnn@gmail.com>
2015-10-09 09:17:33 +0200

    Makefile
    Ho iniziato con i Makefiles un po' più seri.

--- Makefile
+++ Makefile
@@ -1,9 +1,27 @@
+# Per prima cosa le flags globali
+
+# Prima il c++
+export CXXFLAGS := -g -std=c++11
+
+# Poi il linker!
+export LDFLAGS := -lpthread
+
+# Ok ogni makefile farà poi quello che deve per aggiungere quello che serve.
+# Adesso, ogni sottocartella corrisponde ad un target.
+TARGETS := $(notdir $(wildcard source/*)) # Con questo prendo tutti i nomi delle cartelle
+
+# Ok adesso eseguo all, che l'unica cosa che fa è dipendere da tutti i target.
+all: $(TARGETS)
+	
+
+# Ogni target a sua volta chiama il costruttore vero nella cartella build
+# e dipende da tutti i sorgenti presenti nella cartella di destinazione.
+%: source/%/*
+	$(MAKE) -C build $@
+
+# clean è da eseguire sempre.
+.PHONY: clean
 
 
-
-all: test source
-
-source:
-
-test:
-	$(MAKE) -C source/test all
+clean:
+	$(RM) -rf build/*
---
+++ source/utilities/include/threadpool.hpp
@@ -0,0 +1,49 @@
+#ifndef SOURCE_UTILITIES_THREADPOOL_HPP_
+#define SOURCE_UTILITIES_THREADPOOL_HPP_
+
+#include <functional>
+#include <future>
+#include <thread>
+
+
+namespace utilities {
+
+	/**
+	 * template thread_pool, che abbiamo preferito rispetto ad una classe
+	 * virtuale perchè così si evita l'eventuale passaggio da una v-table.
+	 *
+	 */
+	template <typename POLICY, typename A, typename...T>
+	class thread_pool : public thread_pool<POLICY, T...> {
+	public:
+		static std::future<A> execute(std::function<A()> f) {
+			return POLICY::dispatch(f);
+		}
+	};
+
+	template <typename POLICY,typename A>
+	class thread_pool<POLICY, A> {
+	public:
+
+		static std::future<A> execute(std::function<A()> f) {
+			return POLICY::dispatch(f);
+		}
+	};
+
+	/**
+	 * Questa classe implementa a tutti gli effetti la politica della
+	 * thread pool. In questo caso lascia che sia il c++ a decidere
+	 * come eseguire i compiti affidatigli.
+	 * FIXME: il nome che ho usato per la funzione (dispatch) non mi piace.
+	 * 		però è meglio di utilizzare l'operatore () perchè se no ci si
+	 * 		confonde poi con i functor/callable e non mi piace.
+	 */
+	struct default_threa_pool_policy {
+		template <typename T>
+		static std::future<T> dispatch(std::function<T()> f) {
+			return std::async(f);
+		}
+	};
+
+}
+#endif
