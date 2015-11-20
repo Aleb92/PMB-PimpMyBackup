05785fbd677da5a235fe305836a0d17114999c44
Marco Manino <mrcmnn@gmail.com>
2015-11-20 14:31:19 +0100

    Cazzo

--- source/utilities/include/fsutil.hpp
+++ source/utilities/include/fsutil.hpp
@@ -14,11 +14,12 @@ namespace utilities{
 	int createDirectoryRecursively(const wchar_t* path);
 	bool pathExists(const wchar_t*path);
 	bool isPathDir(const wchar_t*path);
-	bool fileMD5(const wchar_t*path, unsigned char buff[MD5_DIGEST_LENGTH]);
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+	bool fileMD5(const wchar_t*path, unsigned char buff[MD5_DIGEST_LENGTH]);
 	bool fdMD5(HANDLE hwnd, unsigned char buff[MD5_DIGEST_LENGTH]);
 #else
+	bool fileMD5(const char*path, unsigned char buff[MD5_DIGEST_LENGTH]);
 	bool fdMD5(int fd, unsigned char buff[MD5_DIGEST_LENGTH]);
 #endif
 }
