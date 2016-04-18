65bb40ee78a1390228cb14f8a9972d45c37b8997
mrcmn <mrcmn@DESKTOP-NDVL3F1>
2016-04-18 09:15:47 +0200

    Try-catch nel main e flag per far partire il servizio

--- source/client/main.cpp
+++ source/client/main.cpp
@@ -1,8 +1,10 @@
-
 #include <settings.hpp>
 #include <client.hpp>
+#include <service.hpp>
+
 #include <utilities/include/strings.hpp>
 
+#include <exception>
 #include <iostream>
 #include <iterator>
 
@@ -10,34 +12,43 @@ using namespace std;
 using namespace client;
 
 int main(int argc, char *argv[]) {
+	try {
+		if (argc > 1 && argv[1][0])
+			switch (argv[1][1]) {
+			case 'v':
+				cout
+						<< "Pimp My Backup Client version 0." PMB_VERSION " Copyright (C) 2016  Alessio Bonato and Marco Manino \r\n\r\n"
+						"This program comes with ABSOLUTELY NO WARRANTY; for details see "
+						"<http://www.gnu.org/licenses/gpl.html>.\r\n"
+						"This is free software, and you are welcome to redistribute it"
+						"under certain conditions.\r\n";
+				return 0;
+			case 's':
+				// Se richiesto faccio partire il servizio
+				start_service();
+				return 0;
+			default:
+				cout << "Argument not recgnized...";
+			}
 
-	if(argc > 1 && argv[1][0] == '-' && argv[1][1] == 'v') {
-		cout << "Pimp My Backup Client version 0." PMB_VERSION " Copyright (C) 2016  Alessio Bonato and Marco Manino \r\n\r\n"
-		"This program comes with ABSOLUTELY NO WARRANTY; for details see "
-		"<http://www.gnu.org/licenses/gpl.html>.\r\n"
-		"This is free software, and you are welcome to redistribute it"
-		"under certain conditions.\r\n";
-		return 0;
-	}else{
-		cout << "Argument not recgnized...";
+		// Se no faccio quì in locale (per il debug)
+		client::client c; // più semplice di così...
+
+		// Inizio
+		c.start();
+
+		// Per ora come segnale di stop, scrivo "q" nella shell
+		char r;
+		while ((cin >> r))
+			if (r == 'q' || r == 'Q')
+				break;
+
+		// E stoppo
+		c.stop();
+	} catch (const exception& ex) {
+		cout << "Error: " << ex.what() << endl;
+	} catch (...) {
+		cout << "Unknown exception thrown" << endl;
 	}
-
-	/*
-	 * Questo codice dovrebbe essere strutturato diversamente per diventare un servizio
-	 * tuttavia (per ora) è il main  a gestire il tutto...
-	 */
-	client::client c; // più semplice di così...
-
-	// Inizio
-	c.start();
-
-	// Per ora come segnale di stop, scrivo "q" nella shell
-	char r;
-	while((cin >> r))
-		if(r == 'q' || r == 'Q')
-			break;
-
-	// E stoppo
-	c.stop();
 }
 
